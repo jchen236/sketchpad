00466f645fe59ff6f191a05d05cf3098045eea2f
Jimmy Chen <jzc8@cornell.edu>
2016-12-22 22:12:49 -0500

    Adjustable pad size

--- js/script.js
+++ js/script.js
@@ -1,21 +1,25 @@
-//Insert prompt
-
-var dimensions = prompt("How many squares per side would you like?");
-var unitSize = 560 / dimensions;
-
 
 $(document).ready( function() {
+  clear();
+  $(".unit").hover(function() {
+    $(this).css("background-color", "#000");
+  });
+});
+
+function draw(dimensions) {
   for(var i = 0; i < dimensions; i++) {
     for(var j = 0; j < dimensions; j++) {
       var unit = $("<div class = \"unit\"></div>");
       unit.appendTo("#container");
     }
   }
+}
+
+function clear() {
+  var dimensions = prompt("How many squares per side would you like?");
+  var unitSize = 560 / dimensions;
+  draw(dimensions);
   $(".unit").css("width", unitSize + "px");
   $(".unit").css("height", unitSize + "px");
-
-  $(".unit").hover(function() {
-    $(this).css("background-color", "#FFF");
-    console.log("wtf");
-  });
-});
+  console.log("clearing");
+}
