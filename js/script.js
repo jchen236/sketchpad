1f410fda2ad1a1acddcc5dd0b746e5bd3a09e82c
Jimmy Chen <jzc8@cornell.edu>
2016-12-22 23:34:36 -0500

    Fix bug with redrawing fresh grid

--- js/script.js
+++ js/script.js
@@ -1,25 +1,34 @@
 
 $(document).ready( function() {
-  clear();
-  $(".unit").hover(function() {
-    $(this).css("background-color", "#000");
-  });
-});
+  clean();
 
-function draw(dimensions) {
-  for(var i = 0; i < dimensions; i++) {
-    for(var j = 0; j < dimensions; j++) {
-      var unit = $("<div class = \"unit\"></div>");
-      unit.appendTo("#container");
+  function draw(dimensions) {
+    for(var i = 0; i < dimensions; i++) {
+      for(var j = 0; j < dimensions; j++) {
+        var unit = $("<div class = \"unit\"></div>");
+        unit.appendTo("#container");
+      }
     }
   }
-}
 
-function clear() {
-  var dimensions = prompt("How many squares per side would you like?");
-  var unitSize = 560 / dimensions;
-  draw(dimensions);
-  $(".unit").css("width", unitSize + "px");
-  $(".unit").css("height", unitSize + "px");
-  console.log("clearing");
-}
+  function clean() {
+    var dimensions = prompt("How many squares per side would you like?");
+    var unitSize = 560 / dimensions;
+    draw(dimensions);
+    $(".unit").css("width", unitSize + "px");
+    $(".unit").css("height", unitSize + "px");
+    $(".unit").hover(function() {
+      $(this).css("background-color", "#000");
+    });
+  };
+
+  $("button").click(function () {
+    var myNode = document.getElementById("container");
+    while (myNode.firstChild) {
+      myNode.removeChild(myNode.firstChild);
+    }
+    clean();
+  });
+
+  
+});
